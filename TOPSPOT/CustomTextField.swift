57094d8e7db19caa343fb0f43a85e8967f93e131
Ludo <ludovic.saussinan@gmail.com>
2017-02-08 19:46:15 +0100

    Login UI done

---
+++ TOPSPOT/CustomTextField.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTextField.swift
+//  TOPSPOT
+//
+//  Created by Ludo on 08/02/2017.
+//  Copyright © 2017 Ludo. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let border = CALayer()
+        border.borderColor = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1.0).cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1.0)
+        
+        border.borderWidth = 1.0
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+        
+    }
+}
